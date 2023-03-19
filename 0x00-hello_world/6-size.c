1acda6df32170b50a78bb49b98707363c1539b06
Ayeni Michael <ayenimichael1998@gmail.com>
2023-03-19 16:39:05 -0700

    add size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %2d byte(s)\n", sizeof(float));
-	fprintf(stderr, "Anything");
+	fprintf(stderr, "[Anything]");
 	return (0);
 }
