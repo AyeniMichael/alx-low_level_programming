3315dd068e1c175f4e71267465e7cb71ebf11637
Ayeni Michael <ayenimichael1998@gmail.com>
2023-03-22 14:11:49 -0700

    add program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char word[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		putchar(word[i]);
+	putchar('\n');
+
+	return (0);
+}
